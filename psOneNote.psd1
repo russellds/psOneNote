38009f2954670f10266ba62cb4df9003355d0fce
russellds <russellds@steelsharktech.com>
2014-08-01 16:28:54 -0400

    Initial commit.

---
+++ psOneNote.psd1
@@ -0,0 +1,9 @@
+@{
+        ModuleVersion='0.1.0'
+        CmdletsToExport='*'
+        ModuleToProcess='psOneNote.psm1'
+        Guid='7c7019ca-4f62-451b-972d-4dc5b5b89f7a'
+        VariablesToExport='*'
+        Description='psOneNote is used to update OneNote from Powershell.'
+        Author='Russell Slater'
+    }
---
+++ psOneNote.psm1
@@ -0,0 +1,15 @@
+﻿[Reflection.Assembly]::LoadWithPartialName("Microsoft.Office.Interop.OneNote") | Out-Null
+
+$oneNote = New-Object -ComObject OneNote.Application
+
+$namespace = @{
+    one = 'http://schemas.microsoft.com/office/onenote/2010/onenote'
+}
+
+$scripts = Get-ChildItem $(Join-Path -Path $psScriptRoot -ChildPath "Scripts") -Recurse
+
+foreach( $script in $scripts) {
+	if(-not $script.PSIsContainer) {
+    	. $script.Fullname
+	}
+}
